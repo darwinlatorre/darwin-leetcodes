88e4939e4ec745b4063fa205ad4583d78ed5b4b4
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2024-04-08 10:36:12 -0500

    Feat: New added main funtion for testing

--- Array-String/MergeSortedArray.java
+++ Array-String/MergeSortedArray.java
@@ -1,16 +1,28 @@
 public class MergeSortedArray {
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int lastNumArray1Pointer = m - 1;
-        int lastNumArray2Pointer = n - 1;
-        int lastPosArray1Pointer = m + n - 1;
+    public static void main(String[] agrs) {
+        int[] array1 = { 1, 2, 3, 0, 0, 0 };
+        int[] array2 = { 2, 5, 6 };
+        merge(array1, 3, array2, 3);
+    }
+
+    public static void merge(int[] nums1Array, int length1Array, int[] nums2Array, int length2Array) {
+        int lastNumArray1Pointer = length1Array - 1;
+        int lastNumArray2Pointer = length2Array - 1;
+        int lastPosArray1Pointer = length1Array + length2Array - 1;
 
         while (lastNumArray2Pointer >= 0) {
-            if (lastNumArray1Pointer >= 0 && nums1[lastNumArray1Pointer] > nums2[lastNumArray2Pointer]) {
-                nums1[lastPosArray1Pointer--] = nums1[lastNumArray1Pointer--];
+            if (lastNumArray1Pointer >= 0 && nums1Array[lastNumArray1Pointer] > nums2Array[lastNumArray2Pointer]) {
+                nums1Array[lastPosArray1Pointer--] = nums1Array[lastNumArray1Pointer--];
             } else {
-                nums1[lastPosArray1Pointer--] = nums2[lastNumArray2Pointer--];
+                nums1Array[lastPosArray1Pointer--] = nums2Array[lastNumArray2Pointer--];
             }
         }
+
+        String printOutArray = "";
+        for (int i = 0; i < nums1Array.length; i++) {
+            printOutArray += nums1Array[i] + " ";
+        }
+        System.out.println(printOutArray);
     }
 }
