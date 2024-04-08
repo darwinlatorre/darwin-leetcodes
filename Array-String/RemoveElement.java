f7849def091f7471e55e3d59f57d9025a8c0adc7
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2024-04-08 10:44:56 -0500

    Feat: New added main funtion for testing

--- Array-String/RemoveElement.java
+++ Array-String/RemoveElement.java
@@ -1,5 +1,12 @@
 public class RemoveElement {
-    public int removeElement(int[] numArray, int numToRemoved) {
+    public static void main(String[] agrs) {
+        int[] array1 = { 0, 1, 2, 2, 3, 0, 4, 2 };
+        int numToRemove = 2;
+
+        removeElement(array1, numToRemove);
+    }
+
+    public static int removeElement(int[] numArray, int numToRemoved) {
         int cotainNumbers = 0;
         int removerPointer = 0;
         while (removerPointer < numArray.length) {
@@ -8,6 +15,14 @@ public class RemoveElement {
             }
             removerPointer++;
         }
+
+        String printOutArray = "";
+        for (int i = 0; i < cotainNumbers; i++) {
+            printOutArray += numArray[i] + " ";
+        }
+        System.out.println("New array with removed numbers: " + printOutArray);
+        System.out.println("Count of contained numbers: " + cotainNumbers);
+
         return cotainNumbers;
     }
 }
